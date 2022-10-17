01a8d2e6472ad087b52f5206c59ad99c272e0792
Carine04 <92787751+Carine04@users.noreply.github.com>
2022-10-17 16:07:32 +0200

    Voici mon exam videocenter

---
+++ migrations/Version20221017094022.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221017094022 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'ajout d’un attribut is_verified et de propriété dans l’entité User';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP is_verified');
+    }
+}
